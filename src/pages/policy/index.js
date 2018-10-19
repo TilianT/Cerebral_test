88b52c9db3b81171f8b84ee71f42da9336df38e5
ilya.lysenko <i.lysenkowork@gmail.com>
2018-10-19 17:24:06 +0300

    fifth

--- src/pages/policy/index.js
+++ src/pages/policy/index.js
@@ -1,14 +1,176 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
+let arr1 = [
+  'Privacy',
+  'Policy'
+]
+let arr2 = [
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+  'Privacy',
+  'Policy',
+]
+
 export default () => {
   window.scrollTo(0, 0);
   return (
     <Container>
-      <h1><Span1><Span6>Privacy</Span6></Span1> <Span2><Span7>Policy</Span7></Span2></h1>
+      {
+        arr1.map( item => {
+          const rand1 = Math.floor(Math.random() * 1000);
+          const rand2 = Math.floor(Math.random() * 300);
+          const rand3 = Math.floor(Math.random() * 100);
+          const rand4 = Math.floor(Math.random() * 400);
+          const rand5 = Math.floor(Math.random() * 25);
+          const rand6 = Math.floor(Math.random() * 25);
+
+          const rotate1 = keyframes`
+            from {
+              color: violet;
+              transform: rotate(${rand3});
+            }
+
+            to {
+              color: cyan;
+              transform: rotate(${rand1}deg);
+            }
+          `;
+
+          const rotate2 = keyframes`
+            0% {
+              color: rosybrown;
+              transform: translateY(${rand2});
+              transform: translateX(-${rand6});
+            }
+            25% {
+              transform: translate(${rand2}px);
+            }
+            50% {
+              transform: translateY(-${rand3}px);
+            }
+            100% {
+              color: teal;
+              transform: translateY(${rand4}px);
+              transform: translatex(${rand2}px);
+            }
+          `;
+
+
+          const Span1 = styled.div`
+            width: ${rand6}px !important;
+            animation: ${rotate1} ${rand5}s ease infinite !important;
+          `
+          const Span2 = styled.div`
+            width: ${rand2}px !important;
+            animation: ${rotate2} ${rand6}s ease-in-out infinite !important;
+          `
+
+          return <h1><Span1><Span2>item</Span2></Span1></h1>
+        })
+      }
       <Wrap>
-        <Span3><Span5>Lorem</Span5></Span3>
-        <Span4>Lorem</Span4>
+        {
+          arr2.map( ( item, index ) => {
+            const rand1 = Math.floor(Math.random() * 1000);
+            const rand2 = Math.floor(Math.random() * 300);
+            const rand3 = Math.floor(Math.random() * 300);
+            const rand4 = Math.floor(Math.random() * 300);
+            const rand5 = Math.floor(Math.random() * 11);
+            const rand6 = Math.floor(Math.random() * 11);
+
+            const rotate1 = keyframes`
+            from {
+              color: violet;
+              transform: rotate(0deg);
+            }
+
+            to {
+              color: cyan;
+              transform: rotate(${rand1}deg);
+            }
+          `;
+
+            const rotate2 = keyframes`
+            0% {
+              color: sandybrown;
+              transform: translateY(-50px);
+              transform: translateX(-50px);
+            }
+            25% {
+              transform: translate(${rand2}px);
+            }
+            50% {
+              transform: translateY(${rand3}px);
+            }
+            100% {
+              color: red;
+              transform: translateY(${rand4}px);
+              transform: translatex(${rand2}px);
+            }
+          `;
+
+            const Span1 = styled.div`
+            width: ${rand6}px !important;
+            font-size: 12px;
+            animation: ${rotate1} ${rand5}s ease infinite !important;
+          `
+            const Span2 = styled.div`
+            width: ${rand2}px !important;
+            animation: ${rotate2} ${rand6}s ease-in-out infinite !important;
+          `
+
+
+            return (
+              <Span1><Span2>item</Span2></Span1>
+            )
+          })
+        }
+
       </Wrap>
     </Container>
   )
@@ -23,132 +185,11 @@ const Container = styled.div`
   margin: 0 auto;
   max-width: 768px;
   display: flex;
+  justify-content: center;
   flex-direction: column;
+  align-items: center;
   
-  text-align: left;
+  text-align: center;
   
   color: #333;
 `;
-
-const rotate1 = keyframes`
-  from {
-    color: violet;
-    transform: rotate(0deg);
-  }
-
-  to {
-    color: cyan;
-    transform: rotate(360deg);
-  }
-`;
-const rotate2 = keyframes`
-  from {
-    color: chartreuse;
-    transform: rotate(0deg);
-  }
-
-  to {
-    color: fuchsia;
-    transform: rotate(-370deg);
-  }
-`;
-const rotate3 = keyframes`
-  0% {
-    color: rosybrown;
-    transform: translateY(-50%);
-    transform: translate(-50%);
-  }
-  25% {
-    transform: translate(150%);
-  }
-  50% {
-    transform: translateY(350px);
-  }
-  100% {
-    color: teal;
-    transform: translateY(0%);
-    transform: translateX(-50%);
-  }
-`;
-const rotate6 = keyframes`
-  0% {
-    color: rosybrown;
-    transform: translateY(-150px);
-    transform: translateX(-150px);
-  }
-  25% {
-    transform: translate(450px);
-  }
-  50% {
-    transform: translateY(350px);
-  }
-  100% {
-    color: teal;
-    transform: translateY(0%);
-    transform: translateX(50%);
-  }
-`;
-const rotate7 = keyframes`
-  0% {
-    color: rosybrown;
-    transform: translateY(150px);
-    transform: translateX(-150px);
-  }
-  25% {
-    transform: translateY(250px);
-    transform: translateX(150px);
-  }
-  50% {
-    transform: translateY(650px);
-  }
-  100% {
-    color: teal;
-    transform: translateY(10px);
-    transform: translateX(-50px);
-  }
-`;
-const rotate4 = keyframes`
-  from {
-    color: rosybrown;
-    transform: rotate(-100deg);
-  }
-
-  to {
-    color: teal;
-    transform: rotate(2760deg);
-  }
-`;
-
-const Span1 = styled.div`
-  width: 400px;
-  animation: ${rotate1} 4s ease infinite;
-`
-const Span2 = styled.div`
-  width: 10px;
-  animation: ${rotate2} 3s ease-in-out infinite;
-`
-const Span3 = styled.div`
-  width: 500px;
-  font-size: 30px;
-  animation: ${rotate3} 10s ease infinite;
-`
-const Span4 = styled.div`
-  width: 200px;
-  font-size: 70px;
-  animation: ${rotate4} 2s ease infinite;
-`
-const Span5 = styled.div`
-  width: 50px;
-  font-size: 30px;
-  animation: ${rotate4} 7s ease infinite;
-`
-const Span6 = styled.div`
-  width: 50px;
-  font-size: 50px;
-  animation: ${rotate6} 5s ease infinite;
-`
-const Span7 = styled.div`
-  width: 350px;
-  font-size: 42px;
-  animation: ${rotate7} 8s ease infinite;
-`
